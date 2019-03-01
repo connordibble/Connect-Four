40068d4962e80e5afabf27b2ffc6208a0760c871
connor-dibble <connor.dibble@aggiemail.usu.edu>
2019-02-28 22:13:41 -0700

    Added cases for diagonals and did some code refactoring.  Added an animation for when a winner is determined.

--- src/ConnectFour.java
+++ src/ConnectFour.java
@@ -1,35 +1,43 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import javafx.scene.shape.Circle;
 import javafx.scene.paint.Color;
+import javafx.geometry.Insets;
+import java.util.ArrayList;
+import javafx.util.Duration;
+import javafx.animation.Timeline;
+import javafx.animation.KeyFrame;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 
 public class ConnectFour extends Application {
     private int moveNumber = 0;
+    private ArrayList<ArrayList<Circle>> circArray = new ArrayList<>();
 
     public void start(Stage primaryStage) {
-        Pane myPane = new Pane();
+        GridPane myPane = new GridPane();
         myPane.setPrefWidth(390);
         myPane.setPrefHeight(350);
+        myPane.setPadding(new Insets(20,20,20,20));
+        myPane.setVgap(10);
+        myPane.setHgap(10);
 
-        //make this better by putting the x,y and radius in terms of the size of the pane
+
+        //create the connect four grid
         for (int j = 0; j < 6 ; j++) {
+            ArrayList<Circle> circRow = new ArrayList<>();
             for (int i = 0; i < 7; i++) {
-                Circle circ = new Circle(30 + 55 * i, 30 + 55 * j, 25, Color.WHITE);
+                Circle circ = new Circle(30, Color.WHITE);
                 circ.setStroke(Color.BLACK);
-                myPane.getChildren().add(circ);
-
-                circ.setOnMouseClicked(e ->{
-                if (moveNumber % 2 == 0) circ.setFill(Color.BLUE);
-                else circ.setFill(Color.RED);
-                moveNumber++;
-            });
-
+                myPane.add(circ,i,j);
+                circ.setOnMouseClicked(e -> circleLogic(circ));
+                circRow.add(circ);
             }
+            circArray.add(circRow);
         }
-
-
+        System.out.println(myPane);
 
         Scene scene = new Scene(myPane);
         primaryStage.setTitle("Connect Four"); // Set the stage title
@@ -38,6 +46,139 @@ public class ConnectFour extends Application {
 
     }
 
+    private void circleLogic (Circle circ){
+        //determine which color to make the circle upon click
+        if (moveNumber % 2 == 0) circ.setFill(Color.BLUE);
+        else circ.setFill(Color.RED);
+        moveNumber++;
 
+        //check all cases for wins using methods below
+        checkUp();
+        checkHorizontal();
+        checkDiagonalRight();
+        checkDiagonalLeft();
+    }
+
+    private void checkUp(){
+        int winNumberUpRed = 0;
+        int winNumberUpBlue = 0;
+
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 6; j++){
+                if (this.circArray.get(j).get(i).getFill() == Color.BLUE ) winNumberUpBlue++;
+                else winNumberUpBlue = 0;
+
+                if (this.circArray.get(j).get(i).getFill() == Color.RED ) winNumberUpRed++;
+                else winNumberUpRed = 0;
+
+                //TDL Break it off when a winner is found
+                if (winNumberUpRed == 4 || winNumberUpBlue == 4){
+
+                        animation(this.circArray.get(j).get(i));
+                        animation(this.circArray.get(j - 1).get(i));
+                        animation(this.circArray.get(j - 2).get(i));
+                        animation(this.circArray.get(j - 3).get(i));
+                        winNumberUpBlue = 0;
+                        winNumberUpRed = 0;
+                    }
+                }
+            }
+        }
+
+
+    private void checkHorizontal(){
+        int winNumberHorizontalBlue = 0;
+        int winNumberHorizontalRed = 0;
+
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 7; j++){
+                if (this.circArray.get(i).get(j).getFill() == Color.BLUE ) winNumberHorizontalBlue++;
+                else winNumberHorizontalBlue = 0;
+
+                if (this.circArray.get(i).get(j).getFill() == Color.RED ) winNumberHorizontalRed++;
+                else winNumberHorizontalRed = 0;
+
+
+                if (winNumberHorizontalBlue == 4 || winNumberHorizontalRed == 4){
+
+                        animation(this.circArray.get(i).get(j));
+                        animation(this.circArray.get(i).get(j - 1));
+                        animation(this.circArray.get(i).get(j - 2));
+                        animation(this.circArray.get(i).get(j - 3));
+                        winNumberHorizontalRed = 0;
+                        winNumberHorizontalBlue = 0;
+                    }
+                }
+            }
+        }
+
+
+    private void checkDiagonalRight(){
+        int winNumberDiagRightRed = 0;
+        int winNumberDiagRightBlue = 0;
+
+        for (int i = 0; i < 3 ; i++) {
+            for (int j = 0; j < 4; j++) {
+                for (int k = 0; k < 4; k++) {
+                    if (this.circArray.get(i + k).get(j + k).getFill() == Color.BLUE) winNumberDiagRightBlue++;
+
+                    if (this.circArray.get(i + k).get(j + k).getFill() == Color.RED) winNumberDiagRightRed++;
+
+                    if (winNumberDiagRightBlue == 4 || winNumberDiagRightRed == 4){
+                        animation(this.circArray.get(i + k).get(j + k));
+                        animation(this.circArray.get(i + k - 1).get(j + k - 1));
+                        animation(this.circArray.get(i + k - 2).get(j + k - 2));
+                        animation(this.circArray.get(i + k - 3).get(j + k - 3));
+                    }
+                }
+                winNumberDiagRightRed = 0;
+                winNumberDiagRightBlue = 0;
+            }
+        }
+    }
+
+
+    private void checkDiagonalLeft(){
+        int winNumberDiagLeftRed = 0;
+        int winNumberDiagLeftBlue = 0;
+
+        for (int i = 0; i < 3 ; i++) {
+            for (int j = 4; j < 7; j++) {
+                for (int k = 0; k < 4; k++) {
+                    if (this.circArray.get(i + k).get(j - k).getFill() == Color.BLUE) winNumberDiagLeftBlue++;
+
+                    if (this.circArray.get(i + k).get(j - k).getFill() == Color.RED) winNumberDiagLeftRed++;
+
+                    if (winNumberDiagLeftBlue == 4 || winNumberDiagLeftRed == 4){
+                        animation(this.circArray.get(i + k).get(j - k));
+                        animation(this.circArray.get(i + k - 1).get(j - k + 1));
+                        animation(this.circArray.get(i + k - 2).get(j - k + 2));
+                        animation(this.circArray.get(i + k - 3).get(j - k + 3));
+                    }
+                }
+                winNumberDiagLeftRed = 0;
+                winNumberDiagLeftBlue = 0;
+            }
+        }
+
+    }
+
+    //make animation here for when winning move is made
+    private void animation(Circle circ){
+        EventHandler<ActionEvent> eventHandler = e -> {
+            circ.setFill(Color.color(Math.random(),Math.random(),Math.random())); // Set a new clock time
+        };
+
+        Timeline animation = new Timeline(
+                new KeyFrame(Duration.millis(500),eventHandler));
+        animation.setCycleCount(Timeline.INDEFINITE);
+        animation.play(); // Start animation
+    }
 
 }
+
+
+
+
+
+
